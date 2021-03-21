cbb1ec276430a487d39d4d6eb82468b55b82687e
mmayes098 <mayes.matthewj@gmail.com>
2021-03-21 19:32:51 -0400

    Finished project

--- javascript.js
+++ javascript.js
@@ -1,7 +1,14 @@
-var currentNumber = 0;
+var currentDisplay = '';
+var currentOperator = "";
+var numA = 0;
+var numB = 0;
+const display = document.getElementById("display");
+const child = document.getElementById("output");
+const div = document.createElement('div');
+div.setAttribute('id', 'output');
 
 function add(a, b) {
-    var result = a + b;
+    var result = Number(a) + Number(b);
     return result;
 }
 
@@ -25,6 +32,15 @@ function divide(a, b) {
     return result;
 }
 
+function clearDisplay(a) {
+    numA = 0;
+    numB = 0;
+    currentDisplay = '';
+    div.textContent = 'Welcome!';
+    display.removeChild(child);
+    display.appendChild(div);
+}
+
 function operate(a, b, operator) {
     var result = 0;
     if (operator == "add") {
@@ -37,15 +53,44 @@ function operate(a, b, operator) {
         result = divide(a, b);
     }
 
-    return result;
+    div.textContent = result;
+    display.removeChild(child);
+    display.appendChild(div);
 }
 
 function updateDisplay(a) {
-    const display = document.getElementById("display");
-    const child = document.getElementById("output");
-    const div = document.createElement('div');
-    div.setAttribute('id', 'output');
-    div.textContent = a;
+    if (a == 'add') {
+        numA = currentDisplay;
+        currentOperator = 'add';
+        div.textContent = '+';
+        currentDisplay = '';
+    } else if (a == 'subtract') {
+        numA = currentDisplay;
+        currentOperator = 'subtract';
+        div.textContent = '-';
+        currentDisplay = '';
+    } else if (a == 'multiply') {
+        numA = currentDisplay;
+        currentOperator = 'multiply';
+        div.textContent = 'X';
+        currentDisplay = '';
+    } else if (a == 'divide') {
+        numA = currentDisplay;
+        currentOperator = 'divide';
+        div.textContent = '/';
+        currentDisplay = '';
+    } else {
+        div.textContent = currentDisplay + a;
+        currentDisplay += a;
+    }
+
     display.removeChild(child);
     display.appendChild(div);
+}
+
+function equals() {
+    numB = currentDisplay;
+    currentDisplay = '';
+    
+    operate(numA, numB, currentOperator);
 }
